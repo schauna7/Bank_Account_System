713d3d8bcb724b82d000c374867288617a262148
Preeti <preetihatapaki@gmail.com>
2026-02-13 09:48:12 +0530

    Test commit on test-branch

--- src/add.java
+++ src/add.java
@@ -1,18 +1,9 @@
 public class add {
-<<<<<<< HEAD
-    public int addNumbers(int a, int b) {
-       int sum=0;
-         sum=a+b;
-         System.out.println("The sum is :"+sum);
-         return sum;
-    }
-    
-=======
     public int addNumbers(int x, int y){
         int sum = 0;
         sum = x + y;
         System.out.println("The sum is" +sum);
         return sum;
     }
->>>>>>> 78f5310b0be8820ae1cd40753f02a13e4a4e4a1c
 }
+// Testing new branch
